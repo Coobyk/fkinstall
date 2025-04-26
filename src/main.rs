67dcb9abec6e12f62c07c896da0f942e1f15473c
Coobyk <110892785+Coobyk@users.noreply.github.com>
2025-04-26 17:03:05 +0200

    Add search functionality

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,14 @@ struct Args {
     install: Option<String>,
     #[clap(short, long, help = "Search term")]
     search: Option<String>,
+    #[clap(short, long, help = "List all available programs")]
+    list: Option<String>,
     #[clap(short, long, help = "Update installed programs")]
     update: Option<String>,
 }
 
 #[derive(serde::Deserialize)]
-struct Apps {
+struct App {
     name: String,
     url: String,
 }
@@ -44,7 +46,7 @@ fn check_config(conf_dir: &str, conf_file: &str) {
     }
 }
 
-fn fetch_list(conf_file: &str) -> Vec<Apps> {
+fn fetch_list(conf_file: &str) -> Vec<App> {
     let conf_file = fs::read_to_string(conf_file).expect("Could not read config");
     let lines = conf_file.lines();
     let mut url = String::new();
@@ -57,8 +59,8 @@ fn fetch_list(conf_file: &str) -> Vec<Apps> {
     let body = response.text().expect("Failed to read body");
     let apps_json: serde_json::Value =
         serde_json::from_str(&body).expect("Failed to parse apps list as json");
-    let apps_vec: Vec<Apps> = serde_json::from_value(apps_json["apps"].clone())
-        .expect("Failed to parse apps list as Vec<Apps>");
+    let apps_vec: Vec<App> = serde_json::from_value(apps_json["apps"].clone())
+        .expect("Failed to parse apps list as Vec<App>");
     for app in &apps_vec {
         println!("{}", app.name);
     }
@@ -66,11 +68,20 @@ fn fetch_list(conf_file: &str) -> Vec<Apps> {
 }
 
 fn main() {
-    Args::parse();
+    let args = Args::parse();
     // Get $HOME
     let home = env::var("HOME").expect("Could not determine home directory");
     let conf_dir = format!("{}/.config", home);
     let conf_file = format!("{}/fkinstall.conf", conf_dir);
     check_config(&conf_dir, &conf_file);
     let project_list = fetch_list(&conf_file);
+    if let Some(search_term) = &args.search {
+        println!("Search results for '{}' :", search_term);
+        for app in project_list
+            .iter()
+            .filter(|app| app.name.contains(search_term))
+        {
+            println!("{}", app.name);
+        }
+    }
 }
